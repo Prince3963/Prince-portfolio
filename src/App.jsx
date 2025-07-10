193e0db10a4a93b2e8338a9ae6623cc026bab1f6
Prince3963 <princepatel3963@gmail.com>
2025-07-10 17:41:50 +0530

    code to noted

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
 import Navbar from './Component/Navbar';
 import Home from './Component/Home';
-import About from './Component/About';
-import Skills from './Component/Skill';
-import Projects from './Component/Projects';
-import Contact from './Component/Contact';
-import ScrollToTop from './Component/ScrollToTop';
 
 function App() {  
   
